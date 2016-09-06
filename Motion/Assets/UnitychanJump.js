68e0eebf864d284180856cd417ee7bb5de82e7dd
tokuhisa4 <tokuhisa4@gmail.com>
2016-09-06 13:29:06 +0900

    Unityちゃん Jump

---
+++ Motion/Assets/UnitychanJump.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+    var myAnimator=GetComponent(Animator);       
+    if (Input.GetKey(KeyCode.Space))
+    {
+        myAnimator.SetBool("Jump",true);            
+    }
+
+    var state:AnimatorStateInfo=myAnimator.GetCurrentAnimatorStateInfo(0);
+    if(state.IsName("Locomotion.Jump"))
+    {
+        myAnimator.SetBool("Jump",false);
+    }
+
+}
