35d734ab839f6444fcf7dce87bee62429090c867
tokuhisa4 <tokuhisa4@gmail.com>
2016-09-06 17:51:16 +0900

    メインに反映

---
+++ Internship/Assets/UnitychanHandsUp.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+ var myAnimator=GetComponent(Animator);     
+ if (Input.GetKey(KeyCode.H))
+ {
+  myAnimator.SetBool("HandsUp",true);            
+ }
+
+ var state:AnimatorStateInfo=myAnimator.GetCurrentAnimatorStateInfo(0);
+ if(state.IsName("Locomotion.HandsUp"))
+  {
+    myAnimator.SetBool("HandsUp",false);         
+  }
+ }
---
+++ Internship/Assets/UnitychanJump.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+    var myAnimator=GetComponent(Animator);
+    var state:AnimatorStateInfo=myAnimator.GetCurrentAnimatorStateInfo(0);
+    
+    if (Input.GetKey(KeyCode.Space) && state.IsName("Locomotion.WalkRun"))
+    {
+        myAnimator.SetBool("Jump",true);            
+    }
+
+    if(state.IsName("Locomotion.Jump"))
+    {
+        myAnimator.SetBool("Jump",false);
+    }
+
+}
